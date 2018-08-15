f42ecee531886a4da570bb261c0ac3f0a9b2e27e
PaulLiu <paul.liu@ams.co.nz>
2018-08-15 13:51:25 +1200

    Add simple settings page

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ export class App {
     configureRouter(config, router){
         config.title = 'Aurelia Notes';
         config.map([
-            { route: '', moduleId: PLATFORM.moduleName('welcome') }
+            { route: '', moduleId: PLATFORM.moduleName('welcome') },
+            { route: 'settings', moduleId: PLATFORM.moduleName('settings/index'), 
+              title: 'Setting' }
         ]);
 
         this.router = router;
