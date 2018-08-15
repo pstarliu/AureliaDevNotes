8201c79cc60b18cc59fb26b306a95e74bb3f4b33
PaulLiu <paul.liu@ams.co.nz>
2018-08-15 13:20:01 +1200

    Add welcome screen and module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+import { PLATFORM } from "aurelia-framework"
 export class App {
     configureRouter(config, router){
         config.title = 'Aurelia Notes';
         config.map([
-            { route: '', moduleId: 'welcome' }
+            { route: '', moduleId: PLATFORM.moduleName('welcome') }
         ]);
 
         this.router = router;
